405b93455df2b2cff8079c4c4d0a5b92f0434db1
hugoalh <32359235+hugoalh@users.noreply.github.com>
2020-11-07 15:25:23 +0800

    Finalize main

--- main.js
+++ main.js
@@ -346,172 +346,144 @@ const advancedDetermine = require("@hugoalh/advanced-determine"),
 			variableSystem.list.external[keyExternal]
 		);
 	});
-
-
-
-	// TODO
-	githubAction.core.debug(`Construct payload content. ([GitHub Action] Send To Discord)`);
-	let output = {
-		allowed_mentions: {
-			parse: [
-				"roles",
-				"users",
-				"everyone"
-			]
-		}
-	};
-	if (advancedDetermine.isBoolean(inputCannotVariable.messageUseTextToSpeech, { allowStringify: true }) !== true) {
-		throw new TypeError(`Argument "message_usetexttospeech" must be type of boolean! ([GitHub Action] Send To Discord)`);
-	};
-	output.tts = (inputCannotVariable.messageUseTextToSpeech === "true");
-	if (advancedDetermine.isString(inputCanVariable.webhookName) === true && inputCanVariable.webhookName.length >= 2 && inputCanVariable.webhookName.length <= 32) {
-		output.username = inputCanVariable.webhookName;
-	};
-	if (advancedDetermine.isString(inputCanVariable.webhookAvatarUrl) === true) {
-		output.avatar_url = inputCanVariable.webhookAvatarUrl;
-	};
-	if (advancedDetermine.isString(inputCanVariable.messageText) === true) {
-		if (inputCanVariable.messageText.length > 2000) {
-			inputCanVariable.messageText = `${inputCanVariable.messageText.slice(0, 1996)}...`;
+	githubAction.core.info(`Analysis configuration argument (stage 2). ([GitHub Action] Send To Discord)`);
+	if (advancedDetermine.isString(delta.content) === true) {
+		if (delta.content.length > 2000) {
+			delta.content = `${delta.content.slice(0, 1996)}...`;
+			githubAction.core.isDebug(`Content: ${delta.content} ([GitHub Action] Send To Discord)`);
 		};
-		output.content = inputCanVariable.messageText;
 	};
-	if (advancedDetermine.isString(inputCanVariable.messageEmbedAuthorName) === true ||
-		advancedDetermine.isString(inputCanVariable.messageEmbedTitle) === true ||
-		advancedDetermine.isString(inputCanVariable.messageEmbedDescription) === true ||
-		advancedDetermine.isString(inputCanVariable.messageEmbedThumbnailUrl) === true ||
-		advancedDetermine.isString(inputCanVariable.messageEmbedImageUrl) === true ||
-		advancedDetermine.isString(inputCanVariable.messageEmbedVideoUrl) === true ||
-		advancedDetermine.isString(inputMessageEmbedFields) === true ||
-		advancedDetermine.isString(inputCanVariable.messageEmbedFooterText) === true
-	) {
-		output.embeds = [{}];
-		if (advancedDetermine.isString(inputCannotVariable.messageEmbedColour) !== true) {
-			throw new TypeError(`Argument "message_embed_colour" must be type of string (non-nullable)! ([GitHub Action] Send To Discord)`);
+	if (advancedDetermine.isString(delta.username) === true) {
+		if (delta.username.length < 2 || delta.username.length > 32) {
+			githubAction.core.warning(`Configuration argument "username"'s length must larger than 1 and smaller than 33! Ignored this key. ([GitHub Action] Send To Discord)`);
+			delete delta.username;
 		};
-		inputCannotVariable.messageEmbedColour = inputCannotVariable.messageEmbedColour.toUpperCase();
-		let colourRGB = [];
-		switch (inputCannotVariable.messageEmbedColour) {
-			case "RANDOM":
-				colourRGB = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
-				break;
-			case "DISCORDBLURPLE":
-				colourRGB = [114, 137, 218];
-				break;
-			case "WHITE":
-				colourRGB = [255, 255, 255];
-				break;
-			case "BLACK":
-				colourRGB = [0, 0, 0];
-				break;
-			case "DISCORDGREYPLE":
-				colourRGB = [153, 170, 181];
-				break;
-			case "DISCORDDARK":
-				colourRGB = [44, 47, 51];
-				break;
-			case "DISCORDBLACK":
-				colourRGB = [35, 39, 42];
-				break;
-			default:
-				if (inputCannotVariable.messageEmbedColour.search(/^[0-9]{1,3},[0-9]{1,3},[0-9]{1,3}$/u) !== 0) {
-					throw new SyntaxError(`Argument "message_embed_colour"'s value is not an expected colour scheme! Read the documentation for more information. ([GitHub Action] Send To Discord)`);
+	};
+	if (advancedDetermine.isArray(delta.embeds) === true) {
+		let characterCount = 0;
+		delta.embeds.forEach((embed, indexEmbed) => {
+			if (advancedDetermine.isString(delta.embeds[indexEmbed].title) === true) {
+				if (delta.embeds[indexEmbed].title.length > 256) {
+					delta.embeds[indexEmbed].title = `${delta.embeds[indexEmbed].title.slice(0, 253)}...`;
+					githubAction.core.isDebug(`Embed#${indexEmbed} Title: ${delta.embeds[indexEmbed].title} ([GitHub Action] Send To Discord)`);
 				};
-				inputCannotVariable.messageEmbedColour = inputCannotVariable.messageEmbedColour.split(",");
-				colourRGB = [
-					Number(inputCannotVariable.messageEmbedColour[0]),
-					Number(inputCannotVariable.messageEmbedColour[1]),
-					Number(inputCannotVariable.messageEmbedColour[2])
-				];
-				colourRGB.forEach((element) => {
-					if (
-						advancedDetermine.isNumberPositiveInteger(element) !== true ||
-						element > 255
-					) {
-						throw new RangeError(`Argument "message_embed_colour"'s value is not a RGB standard! Read the documentation for more information. ([GitHub Action] Send To Discord)`);
+				characterCount += delta.embeds[indexEmbed].title.length;
+			};
+			if (advancedDetermine.isString(delta.embeds[indexEmbed].description) === true) {
+				if (delta.embeds[indexEmbed].description.length > 2048) {
+					delta.embeds[indexEmbed].description = `${delta.embeds[indexEmbed].description.slice(0, 2045)}...`;
+					githubAction.core.isDebug(`Embed#${indexEmbed} Description: ${delta.embeds[indexEmbed].description} ([GitHub Action] Send To Discord)`);
+				};
+				characterCount += delta.embeds[indexEmbed].description.length;
+			};
+			if (advancedDetermine.isString(delta.embeds[indexEmbed].color) === true) {
+				let colorNode = delta.embeds[indexEmbed].color.toLowerCase(),
+					colorRGB = [];
+				switch (colorNode) {
+					case "random":
+						colorRGB = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
+						break;
+					case "discordblurple":
+						colorRGB = [114, 137, 218];
+						break;
+					case "white":
+						colorRGB = [255, 255, 255];
+						break;
+					case "black":
+						colorRGB = [0, 0, 0];
+						break;
+					case "discordgreyple":
+						colorRGB = [153, 170, 181];
+						break;
+					case "discorddark":
+						colorRGB = [44, 47, 51];
+						break;
+					case "discordblack":
+						colorRGB = [35, 39, 42];
+						break;
+					default:
+						if (colorNode.search(/^[0-9]{1,3},[0-9]{1,3},[0-9]{1,3}$/u) !== 0) {
+							throw new SyntaxError(`Configuration argument "embed#${indexEmbed}.color"'s value is not an expected colour scheme! Read the documentation for more information. ([GitHub Action] Send To Discord)`);
+						};
+						colorNode = colorNode.split(",");
+						colorRGB = [
+							Number(colorNode[0]),
+							Number(colorNode[1]),
+							Number(colorNode[2])
+						];
+						colorRGB.forEach((element) => {
+							if (
+								advancedDetermine.isNumberPositiveInteger(element) !== true ||
+								element > 255
+							) {
+								throw new RangeError(`Configuration argument "embed#${indexEmbed}.color"'s value is not a RGB standard! Read the documentation for more information. ([GitHub Action] Send To Discord)`);
+							};
+						});
+						break;
+				};
+				delta.embeds[indexEmbed].color = colorRGB[0] * 65536 + colorRGB[1] * 256 + colorRGB[2];
+			};
+			if (advancedDetermine.isNumberPositiveInteger(delta.embeds[indexEmbed].color) === true) {
+				if (delta.embeds[indexEmbed].color > 16777215) {
+					throw new RangeError(`Configuration argument "embed#${indexEmbed}.color"'s value is out of range! Read the documentation for more information. ([GitHub Action] Send To Discord)`);
+				};
+			};
+			if (advancedDetermine.isJSON(delta.embeds[indexEmbed].footer) === true) {
+				if (advancedDetermine.isString(delta.embeds[indexEmbed].footer.text) === true) {
+					if (delta.embeds[indexEmbed].footer.text.length > 2048) {
+						delta.embeds[indexEmbed].footer.text = `${delta.embeds[indexEmbed].footer.text.slice(0, 2045)}...`;
+						githubAction.core.isDebug(`Embed#${indexEmbed} Footer Text: ${delta.embeds[indexEmbed].footer.text} ([GitHub Action] Send To Discord)`);
+					};
+					characterCount += delta.embeds[indexEmbed].footer.text.length;
+				};
+			};
+			if (advancedDetermine.isJSON(delta.embeds[indexEmbed].author) === true) {
+				if (advancedDetermine.isString(delta.embeds[indexEmbed].author.name) === true) {
+					if (delta.embeds[indexEmbed].author.name.length > 256) {
+						delta.embeds[indexEmbed].author.name = `${delta.embeds[indexEmbed].author.name.slice(0, 253)}...`;
+						githubAction.core.isDebug(`Embed#${indexEmbed} Author Name: ${delta.embeds[indexEmbed].author.name} ([GitHub Action] Send To Discord)`);
+					};
+					characterCount += delta.embeds[indexEmbed].author.name.length;
+				};
+			};
+			if (advancedDetermine.isArray(delta.embeds[indexEmbed].fields) === true) {
+				delta.embeds[indexEmbed].fields.forEach((field, indexField) => {
+					if (advancedDetermine.isJSON(delta.embeds[indexEmbed].fields[indexField]) === true) {
+						if (advancedDetermine.isString(delta.embeds[indexEmbed].fields[indexField].name) === true) {
+							if (delta.embeds[indexEmbed].fields[indexField].name.length > 256) {
+								delta.embeds[indexEmbed].fields[indexField].name = `${delta.embeds[indexEmbed].fields[indexField].name.slice(0, 253)}...`;
+								githubAction.core.isDebug(`Embed#${indexEmbed} Field#${indexField} Name: ${delta.embeds[indexEmbed].fields[indexField].name} ([GitHub Action] Send To Discord)`);
+							};
+							characterCount += delta.embeds[indexEmbed].fields[indexField].name.length;
+						};
+						if (advancedDetermine.isString(delta.embeds[indexEmbed].fields[indexField].value) === true) {
+							if (delta.embeds[indexEmbed].fields[indexField].value.length > 1024) {
+								delta.embeds[indexEmbed].fields[indexField].value = `${delta.embeds[indexEmbed].fields[indexField].value.slice(0, 1021)}...`;
+								githubAction.core.isDebug(`Embed#${indexEmbed} Field#${indexField} Name: ${delta.embeds[indexEmbed].fields[indexField].value} ([GitHub Action] Send To Discord)`);
+							};
+							characterCount += delta.embeds[indexEmbed].fields[indexField].value.length;
+						};
 					};
 				});
-				break;
-		};
-		output.embeds[0].color = colourRGB[0] * 65536 + colourRGB[1] * 256 + colourRGB[2];
-		if (advancedDetermine.isString(inputCanVariable.messageEmbedAuthorName) === true) {
-			if (inputCanVariable.messageEmbedAuthorName.length > 256) {
-				inputCanVariable.messageEmbedAuthorName = `${inputCanVariable.messageEmbedAuthorName.slice(0, 252)}...`;
-			};
-			output.embeds[0].author = {
-				name: inputCanVariable.messageEmbedAuthorName
-			};
-			if (advancedDetermine.isString(inputCanVariable.messageEmbedAuthorAvatarUrl) === true) {
-				output.embeds[0].author.icon_url = inputCanVariable.messageEmbedAuthorAvatarUrl;
-			};
-			if (advancedDetermine.isString(inputCanVariable.messageEmbedAuthorUrl) === true) {
-				output.embeds[0].author.url = inputCanVariable.messageEmbedAuthorUrl;
-			};
-		};
-		if (advancedDetermine.isString(inputCanVariable.messageEmbedTitle) === true) {
-			if (inputCanVariable.messageEmbedTitle.length > 256) {
-				inputCanVariable.messageEmbedTitle = `${inputCanVariable.messageEmbedTitle.slice(0, 252)}...`;
-			};
-			output.embeds[0].title = inputCanVariable.messageEmbedTitle;
-			if (advancedDetermine.isString(inputCanVariable.messageEmbedTitleUrl) === true) {
-				output.embeds[0].url = inputCanVariable.messageEmbedTitleUrl;
-			};
-		};
-		if (advancedDetermine.isString(inputCanVariable.messageEmbedDescription) === true) {
-			if (inputCanVariable.messageEmbedDescription.length > 2048) {
-				inputCanVariable.messageEmbedDescription = `${inputCanVariable.messageEmbedDescription.slice(0, 2044)}...`;
-			};
-			output.embeds[0].description = inputCanVariable.messageEmbedDescription;
-		};
-		if (advancedDetermine.isString(inputCanVariable.messageEmbedFooterText) === true) {
-			if (inputCanVariable.messageEmbedFooterText.length > 2048) {
-				inputCanVariable.messageEmbedFooterText = `${inputCanVariable.messageEmbedFooterText.slice(0, 2044)}...`;
-			};
-			output.embeds[0].footer = {
-				text: inputCanVariable.messageEmbedFooterText
-			};
-			if (advancedDetermine.isString(inputCanVariable.messageEmbedFooterIconUrl) === true) {
-				output.embeds[0].footer.icon_url = inputCanVariable.messageEmbedFooterIconUrl;
-			};
-		};
-		if (advancedDetermine.isString(inputCanVariable.messageEmbedImageUrl) === true) {
-			output.embeds[0].image = {
-				url: inputCanVariable.messageEmbedImageUrl
-			};
-		};
-		if (advancedDetermine.isString(inputCanVariable.messageEmbedThumbnailUrl) === true) {
-			output.embeds[0].thumbnail = {
-				url: inputCanVariable.messageEmbedThumbnailUrl
-			};
-		};
-		if (advancedDetermine.isString(inputCanVariable.messageEmbedVideoUrl) === true) {
-			output.embeds[0].video = {
-				url: inputCanVariable.messageEmbedVideoUrl
-			};
-		};
-		inputMessageEmbedFields.forEach((field, index) => {
-			if (advancedDetermine.isString(field.name) === true) {
-				if (field.name.length > 256) {
-					inputMessageEmbedFields[index].name = `${field.name.slice(0, 252)}...`;
-				};
-			} else {
-				inputMessageEmbedFields[index].name = "-";
-			};
-			if (advancedDetermine.isString(field.value) === true) {
-				if (field.value.length > 1024) {
-					inputMessageEmbedFields[index].value = `${field.value.slice(0, 1020)}...`;
-				};
-			} else {
-				inputMessageEmbedFields[index].value = "-";
 			};
 		});
-		output.embeds[0].fields = inputMessageEmbedFields;
+		if (characterCount > 6000) {
+			throw new Error(`Characters in all fields of embed title, embed description, embed field name, embed fieldvalue, embed footer text, and embed author name must not exceed 6000 characters in total (restricted by Discord)! ([GitHub Action] Send To Discord)`);
+		};
 	};
-	githubAction.core.debug(`Finalize payload content. ([GitHub Action] Send To Discord)`);
-	githubAction.core.debug(`Export payload. ([GitHub Action] Send To Discord)`);
-	let requestPayload = JSON.stringify(output);
-	githubAction.core.debug(`Send network request to Discord. ([GitHub Action] Send To Discord)`);
-	nodeFetch(
+	githubAction.core.info(`Finalize payload content. ([GitHub Action] Send To Discord)`);
+	delta.allowed_mentions = {
+		parse: [
+			"roles",
+			"users",
+			"everyone"
+		]
+	};
+	githubAction.core.info(`Generate network request payload. ([GitHub Action] Send To Discord)`);
+	let requestPayload = JSON.stringify(delta);
+	githubAction.core.debug(`Network Request Payload: ${requestPayload} ([GitHub Action] Send To Discord)`);
+	githubAction.core.info(`Send network request to Discord. ([GitHub Action] Send To Discord)`);
+	let response = await nodeFetch(
 		`https://discord.com/api/webhooks/${webhook.identificationNumber}/${webhook.token}`,
 		{
 			body: requestPayload,
@@ -519,17 +491,22 @@ const advancedDetermine = require("@hugoalh/advanced-determine"),
 			headers: {
 				"Content-Type": "application/json",
 				"Content-Length": requestPayload.length,
-				"User-Agent": `NodeJS/${process.version.replace(/^v/giu, "")} node-fetch/2.6.1 GitHubAction.SendToDiscord(@hugoalh)/2.1.0`
+				"User-Agent": `NodeJS/${process.version.replace(/^v/giu, "")} node-fetch/2.6.1 GitHubAction.SendToDiscord(@hugoalh)/3.0.0`
 			},
 			method: "POST",
 			redirect: "follow"
 		}
 	).catch((error) => {
 		throw error;
-	}).then((response) => {
-		if (Math.floor(Number(response.status) / 100) !== 2) {
-			throw new Error(`Status Code: ${response.status} ([GitHub Action] Send To Discord)`);
-		};
-		githubAction.core.debug(`Status Code: ${response.status} ([GitHub Action] Send To Discord)`);
 	});
+	githubAction.core.info(`Receive network response from Discord. ([GitHub Action] Send To Discord)`);
+	if (response.status !== 200 && response.status !== 204) {
+		githubAction.core.warning(`Receive status code ${response.status}! May cause error in the beyond. ([GitHub Action] Send To Discord)`);
+	};
+	let responseText = await response.text();
+	if (response.ok === true) {
+		githubAction.core.debug(`${response.status} ${responseText} ([GitHub Action] Send To Discord)`);
+	} else {
+		throw new Error(`${response.status} ${responseText} ([GitHub Action] Send To Discord)`);
+	};
 })();
