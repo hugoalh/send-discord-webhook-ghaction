fafa382881cbbb7375603503fc25a472c78980a9
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-04-12 17:31:14 +0800

    Improved bundler

--- bundler.js
+++ bundler.js
@@ -1,26 +1,37 @@
-import { dirname as pathDirName, join as pathJoin } from "node:path";
 import { existsSync as fsExistsSync } from "node:fs";
-import { fileURLToPath } from "node:url";
 import { mkdir as fsMKDir, readdir as fsReadDir, rm as fsRemove, writeFile as fsWriteFile } from "node:fs/promises";
+import { dirname as pathDirname, join as pathJoin } from "node:path";
+import { fileURLToPath } from "node:url";
 import ncc from "@vercel/ncc";
-const root = pathDirName(fileURLToPath(import.meta.url));
+const root = pathDirname(fileURLToPath(import.meta.url));
 const scriptEntryPointFileName = "main.js";
 const inputDirectoryPath = pathJoin(root, "src");
 const inputFilePath = pathJoin(inputDirectoryPath, scriptEntryPointFileName);
 const outputDirectoryPath = pathJoin(root, "dist");
 const outputFilePath = pathJoin(outputDirectoryPath, scriptEntryPointFileName);
-async function getDirectoryItem(directoryPath) {
+async function getDirectoryItem(directoryPath, relativeBasePath) {
+	if (typeof relativeBasePath === "undefined") {
+		relativeBasePath = directoryPath;
+	}
 	try {
-		return await fsReadDir(directoryPath, { withFileTypes: true });
-	} catch {
+		let result = [];
+		for (let item of await fsReadDir(directoryPath, { withFileTypes: true })) {
+			if (item.isDirectory()) {
+				result.push(...await getDirectoryItem(pathJoin(directoryPath, item.name), relativeBasePath));
+			} else {
+				result.push(pathJoin(directoryPath, item.name).slice(relativeBasePath.length + 1).replace(/\\/gu, "/"));
+			}
+		}
+		return result;
+	} catch (error) {
 		return [];
 	}
 }
 
 /* Clean up or initialize output directory (need to await in order to prevent race conditions). */
 if (fsExistsSync(outputDirectoryPath)) {
-	for (const outputFile of await getDirectoryItem(outputDirectoryPath)) {
-		await fsRemove(pathJoin(outputDirectoryPath, outputFile.name), { recursive: true });
+	for (let fileName of await getDirectoryItem(outputDirectoryPath)) {
+		await fsRemove(pathJoin(outputDirectoryPath, fileName), { recursive: true });
 	}
 } else {
 	await fsMKDir(outputDirectoryPath, { recursive: true });
